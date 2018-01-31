083df5724e2e55360f0f5cb04604c6143d78ca31
wellfrog <wellfrog16@gmail.com>
2018-01-31 15:04:13 +0800

    更新默认样式

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,13 @@ export default {
 </script>
 
 <style lang="less">
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+html, body {
+    padding: 0;
+    margin: 0;
 }
 
-.aa {
-    .qq {
-        font-size: 12px;
-    }
+body {
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    font-size: 14px;
 }
 </style>
